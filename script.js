f5d31d47f5851d2c738adb7c2871f418b5b19945
RogueGrim <equan4843@gmail.com>
2024-08-31 12:49:53 +0530

    completed the game

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function getComputerChoice(){
     return pick
 
 }
-
-function getHumanChoice(){
     console.log("1.Rock")
     console.log("2.Paper")
     console.log("3.Scissor")
 
+function getHumanChoice(){
+    
     const choice= prompt("Choose one")
 
     if(choice==1){
@@ -29,4 +29,51 @@ function getHumanChoice(){
     return pick
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice,computerChoice){
+    
+    if(humanChoice=="Rock" && computerChoice=="Scissor" || humanChoice=="Paper" && computerChoice=="Rock" || humanChoice=="Scissor" && computerChoice=="Paper"){
+        console.log("You win, "+humanChoice+" beats "+computerChoice)
+        humanScore++;
+
+    }else if(humanChoice=="Paper" && computerChoice=="Scissor" || humanChoice=="Scissor" && computerChoice=="Rock" || humanChoice=="Rock" && computerChoice=="Paper"){
+        console.log("You Lose, "+computerChoice+" beats "+humanChoice)
+        computerScore++
+    }else{
+        console.log("Its a Draw!")
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+
+function playGame(){
+
+        console.log("Round "+i)
+        playRound(humanChoice,computerChoice);
+        humanChoice=getHumanChoice()
+        computerChoice=getComputerChoice()
+        
+}
+for(i=1;i<=5;i++){
+    playGame();
+}
+DeclareWinner(computerScore,humanScore)
+
+function DeclareWinner(computerScore,humanScore){
+
+    if(humanScore>computerScore){
+        console.log("You win!")
+    }else if(humanScore<computerScore){
+        console.log("You Lose")
+    }else{
+        console.log("Its a tie")
+    }
+
+    console.log("Your Score :"+humanScore)
+    console.log("Computer Score:"+computerScore)
+}
